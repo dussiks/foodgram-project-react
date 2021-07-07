bf06ce7b823169e0da579955986cb3548760f5be
dussiks <dusik007@inbox.ru>
2021-07-07 13:30:10 +0500

    foodgram.models done.

--- foodgram_project/foodgram/models.py
+++ foodgram_project/foodgram/models.py
@@ -1,32 +1,53 @@
 from django.db import models
 
+from users.models import FoodUser
+
 
 class Tag(models.Model):
     name = models.CharField('тег', max_length=50, unique=True, blank=False)
     slug = models.SlugField(max_length=20, unique=True)
-    hex_code = models.CharField(
-        'hex-код тега', unique=True, max_length=7, default='#49B64E'
+    color = models.CharField(
+        'цвет', unique=True, max_length=7, default='#49B64E'
     )
 
     def __str__(self):
-        return self.slug
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'теги'
+        ordering = ('name',)
 
 
 class Ingredient(models.Model):
-    name = models.CharField('ингредиент', max_length=40)
+    name = models.CharField('ингредиент', max_length=40, blank=False)
+    measurement_unit = models.CharField('ЕИ', max_length=20, blank=False)
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name_plural = 'ингредиенты'
+        ordering = ('name',)
 
 
 class Recipe(models.Model):
-    name = models.CharField('рецепт', max_length=40)
-    image = models.ImageField('рисунок')
-    description = models.TextField('описание')
-    time = models.PositiveSmallIntegerField('время готовки, мин')
-    tag = models.ManyToManyField(Tag)
+    author = models.ForeignKey(FoodUser, on_delete=models.CASCADE, null=False)
+    name = models.CharField('рецепт', max_length=40, blank=False)
+    image = models.ImageField('рисунок', upload_to='image/', null=False)
+    text = models.TextField('описание', blank=False)
+    cooking_time = models.PositiveSmallIntegerField('время готовки, мин', blank=False)
+    tag = models.ManyToManyField(Tag, related_name='tags', null=False)
+    ingredient = models.ManyToManyField(Ingredient, through='RecipeIngredient')
 
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name_plural = 'рецепты'
+        ordering = ('name',)
 
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, blank=False)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, blank=False)
+    amount = models.PositiveSmallIntegerField(blank=False)
