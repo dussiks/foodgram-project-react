dd160e9b73bc787951513964ae46b67677cb7f48
Ildus Islamov <islamov.mail@yandex.com>
2021-07-27 23:33:22 +0500

    settings edited

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -65,8 +65,8 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('POSTGRES_DB', default='postgres'),
         'USER': env('POSTGRES_USER', default='postgres'),
         'PASSWORD': env('POSTGRES_PASSWORD', default='postgres'),
         'HOST': env('POSTGRES_HOST', default='db'),
